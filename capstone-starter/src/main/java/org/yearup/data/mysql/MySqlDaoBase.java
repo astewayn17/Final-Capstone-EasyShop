81cf085fb4e1b57f5e373908882f833d5f91695b
Asteway Nigussie <asteway17@gmail.com>
2025-06-23 14:21:17 -0500

    add comments to MySqlDaoBase class

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
@@ -4,17 +4,18 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public abstract class MySqlDaoBase
-{
+public abstract class MySqlDaoBase {
+
+    // Data source property used to obtain a database connection.
     private DataSource dataSource;
 
-    public MySqlDaoBase(DataSource dataSource)
-    {
+    // Dao constructor that initializes the data source.
+    public MySqlDaoBase(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    protected Connection getConnection() throws SQLException
-    {
+    // Makes a connection object from the configured datasource.
+    protected Connection getConnection() throws SQLException {
         return dataSource.getConnection();
     }
 }
