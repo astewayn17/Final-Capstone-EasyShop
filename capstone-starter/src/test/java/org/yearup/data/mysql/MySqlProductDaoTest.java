574d931761596ea61fd2ca7030ec3981f5a2072f
Asteway Nigussie <asteway17@gmail.com>
2025-06-24 10:07:55 -0500

    make search unit test for MySqlProductDaoTest

--- capstone-starter/src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
+++ capstone-starter/src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
@@ -8,24 +8,53 @@ import org.yearup.models.Product;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class MySqlProductDaoTest extends BaseDaoTestClass
-{
+class MySqlProductDaoTest extends BaseDaoTestClass {
+
     private MySqlProductDao dao;
 
     @BeforeEach
-    public void setup()
-    {
+    public void setup() {
         dao = new MySqlProductDao(dataSource);
     }
 
     @Test
-    public void getById_shouldReturn_theCorrectProduct()
-    {
-        // arrange
+    public void search_shouldReturn_theCorrectProduct() {
+        // Arrange
+        Product eraser = new Product(100, "Eraser", new BigDecimal(5.99), 1,
+                "Simple eraser", "Pink", 30, false, "smartphone.jpg");
+        Product pencil = new Product(101, "Pencil", new BigDecimal(1.99), 2,
+                "Simple pencil", "Yellow", 30, false, "smartphone.jpg");
+        Product pen = new Product(102, "Pen", new BigDecimal(3.99), 3,
+                "Simple pen", "Black", 30, false, "smartphone.jpg");
+        dao.create(eraser);
+        dao.create(pencil);
+        dao.create(pen);
+
+        // Act
+        List<Product> result = dao.search(3, new BigDecimal("1.00"), new BigDecimal("5.00"), "Black");
+
+        // Assert
+        assertEquals(1, result.size(), "Should return only the black pen in category 3 between $1.00 and $5.00");
+        assertEquals("Pen", result.get(0).getName());
+    }
+
+    @Test
+    public void listByCategoryId() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void getById_shouldReturn_theCorrectProduct() {
+        // Arrange
         int productId = 1;
         Product expected = new Product()
         {{
@@ -40,11 +69,37 @@ class MySqlProductDaoTest extends BaseDaoTestClass
             setImageUrl("smartphone.jpg");
         }};
 
-        // act
+        // Act
         var actual = dao.getById(productId);
 
-        // assert
+        // Assert
         assertEquals(expected.getPrice(), actual.getPrice(), "Because I tried to get product 1 from the database.");
     }
 
+    @Test
+    public void create() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void update() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void delete() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
 }
