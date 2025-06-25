d0346d0b92edfeb9c4aef254ce0ad8ba013909e3
Asteway Nigussie <asteway17@gmail.com>
2025-06-25 14:05:57 -0500

    add the unit test

---
+++ capstone-starter/src/test/java/org/yearup/data/mysql/MySqlProfileDaoTest.java
@@ -0,0 +1,42 @@
+package org.yearup.data.mysql;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.yearup.models.Profile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MySqlProfileDaoTest extends BaseDaoTestClass {
+
+    private MySqlProfileDao profileDao;
+
+    @BeforeEach
+    public void setup() {
+        profileDao = new MySqlProfileDao(dataSource);
+    }
+
+    @Test
+    public void getByUserId_should_returnTheCorrectProfile() {
+        // Act
+        Profile result = profileDao.getByUserId(1);
+
+        // Assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void update_should_updateTheProfile() {
+        // Arrange
+        Profile updatedProfile = new Profile();
+        updatedProfile.setFirstName("UpdatedName");
+
+        // Act
+        profileDao.update(1, updatedProfile);
+        Profile result = profileDao.getByUserId(1);
+
+        // Assert
+        assertEquals("UpdatedName", result.getFirstName());
+    }
+}
