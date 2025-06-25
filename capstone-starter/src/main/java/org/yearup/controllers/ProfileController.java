c1749b6ae3a7453ccc7af91f5daec3204b2df517
Asteway Nigussie <asteway17@gmail.com>
2025-06-25 13:53:33 -0500

    add the getProfile method

--- capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,15 +1,47 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/profile")
 @CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class ProfileController {
 
     // A profile requires
+    private ProfileDao profileDao;
+    private UserDao userDao;
 
+    // Injected constructor for the profile controller
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
 
+    // GET the profile for the current user. Gets the user info from their user ID
+    @GetMapping
+    public Profile getProfile(Principal principal) {
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+            Profile profile = profileDao.getByUserId(userId);
+            if (profile == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile not found");
+            }
+            return profile;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
 }
